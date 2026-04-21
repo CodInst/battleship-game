2bf33a5cc7201c24b0ab370d11156b0b8a5d937f
Mansour <Mansour@thecreativenerds.ie>
2026-04-21 22:33:57 +0100

    intial introductory text for the game

--- run.py
+++ run.py
@@ -1,1 +1,26 @@
 
+def input_player():
+     """
+    The function frames the input from the players.
+    The intial input is requested, i.e:
+    the player's name, size of the board and number of ships. 
+    """
+
+def new_game():
+    """
+    The function starts a new game. It gives an introductory text.
+    It runs all function of the program.
+    """
+
+    print(
+        """
+        +======================================+
+        | Welcome to ULTIMATE BATTLESHIPS!!    |
+        | Top left corner is row: 0, col:0     |
+        | Select a number for:                 |
+        |     -the board Size: 5,6 or 7        |
+        |     -the number of Ships: 4,5 or 6   |
+        +======================================+
+        """)
+
+new_game()
